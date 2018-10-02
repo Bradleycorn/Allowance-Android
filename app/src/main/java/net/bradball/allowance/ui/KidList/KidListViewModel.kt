2ee150d0946b30245398f3dbea81cb415d5cdde9
brad.ball <brad.ball@twinspires.com>
2018-10-02 10:04:40 -0400

    Add Dagger (WIP), Start re-work of UI design (WIP)

--- app/src/main/java/net/bradball/allowance/ui/KidList/KidListViewModel.kt
+++ app/src/main/java/net/bradball/allowance/ui/KidList/KidListViewModel.kt
@@ -1,14 +1,14 @@
 package net.bradball.allowance.ui.KidList
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
 import net.bradball.allowance.data.repos.KidsRepo
 import net.bradball.allowance.models.Kid
-import javax.inject.Inject
 
-class KidListViewModel @Inject constructor(private val kidsRepo: KidsRepo): ViewModel() {
+class KidListViewModel: ViewModel() {
 
     fun getKidList(): LiveData<List<Kid>> {
-        return kidsRepo.getKids()
+        return KidsRepo.getKids()
     }
 }
